adf700b902409e14da30794570dd9c76c646a7ef
Phuong\Phuong Nguyen <pt7237556@gmail.com>
2025-06-11 05:25:23 +0700

    Added ticket controller and service

--- src/main/java/com/group2/ADN/entity/Ticket.java
+++ src/main/java/com/group2/ADN/entity/Ticket.java
@@ -2,9 +2,13 @@ package com.group2.ADN.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tickets")
 public class Ticket {
--- src/main/java/com/group2/ADN/repository/UserRepository.java
+++ src/main/java/com/group2/ADN/repository/UserRepository.java
@@ -2,10 +2,14 @@ package com.group2.ADN.repository;
 
 
 import com.group2.ADN.entity.User;
+import com.group2.ADN.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    List<User> findByRole(UserRole role);
 }
