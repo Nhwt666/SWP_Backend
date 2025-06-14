a2c0c7683ecf26a5f7bcc061afc39794fa7aa8cb
Phuong\Phuong Nguyen <pt7237556@gmail.com>
2025-06-15 01:34:55 +0700

    Added ticket

---
+++ src/main/java/com/group2/ADN/repository/TicketRepository.java
@@ -0,0 +1,16 @@
+package com.group2.ADN.repository;
+
+import com.group2.ADN.entity.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByCustomer(User Customer);
+
+    List<Ticket> findByStaff(User Staff);
+
+    List<Ticket> findByStatus(TicketStatus status);
+
+    List<Ticket> findByTypeAndMethod(TicketType type, TestMethod method);
+}
